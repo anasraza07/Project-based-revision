7c6d7e22d8c2a4c01da4395b19d170369a66f4ec
anasraza07 <ar470684@gmail.com>
2024-11-25 01:34:05 +0500

    js part 2 practice questions completed

---
+++ PracticeQuestions/app.js
@@ -0,0 +1,97 @@
+// let str = 'always';
+// if ((str[0] === 'a') && (str.length > 3)) {
+//     console.log('good string')
+// } else {
+//     console.log('not a good string')
+// }
+
+// let day = 5;
+// switch (day) {
+//     case 1:
+//         console.log('Monday');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;
+//     case 4:
+//         console.log('Thursday');
+//         break;
+//     case 5:
+//         console.log('Friday');
+//         break;
+//     case 6:
+//         console.log('Saturday');
+//         break;
+//     case 7:
+//         console.log('Sunday');
+//         break;
+//     default :
+//     console.log('Invalid')
+// }
+
+// // JS (Part 2) Practice Questions
+// // Q1:
+// const num = 20;
+// if (num % 10 === 0) {
+//     console.log('good')
+// } else {
+//     console.log('bad')
+// }
+
+// // Q2:
+// const name = prompt('Enter your name');
+// const age = prompt('Enter your age');
+// alert(`${name} is ${age} years old.`)
+
+// // Q3:
+// const quarter = 4;
+// switch (quarter) {
+//     case 1:
+//         console.log('Jan, Feb, March');
+//         break;
+//     case 2:
+//         console.log('April, May, June');
+//         break;
+//     case 3:
+//         console.log('July, August, September');
+//         break;
+//     case 4:
+//         console.log('October, Nov, Dec');
+//         break;
+//     default:
+//         console.log('Invalid')
+// }
+
+// // Q4:
+// const str = 'apples';
+// if ((str[0] == 'a' || str[0] == 'A') && str.length > 5) {
+//     console.log('golden string');
+// }else{
+//     console.log('not a golden string');
+// }
+
+// // Q5:
+// const a = 1200;
+// const b = 600;
+// const c = 160;
+
+// if (a > b && a > c) {
+//     console.log(`${a} is a larger number`);
+// } else if (b > a && b > c) {
+//     console.log(`${b} is a larger number`);
+// } else {
+//     console.log(`${c} is a larger number`);
+// }
+
+// // Q6:
+// const numOne = 27272732;
+// const numTwo = 132733732;
+
+// if (numOne % 10 == numTwo % 10) {
+//     console.log(`${numOne} and ${numTwo} have the same last digit i.e. ${numOne % 10}`);
+// } else {
+//     console.log('numbers dont have the same last digit');
+// }
